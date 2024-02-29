35ddd35204a9e9883c466a62a952136bfcc8ba3a
saemii-24 <hims2419@gmail.com>
2024-03-01 00:43:44 +0900

    Feat: MusicCard  컴포넌트 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,14 @@ export default async function Home() {
   // console.log(data);
 
   return (
-    <main className=' flex-1 bg-[#F8F9FA]'>
-      <div className='container'>
+    <main className=' flex-1 bg-[#F8F9FA] '>
+      <div className='container py-20'>
         <HomeTitle />
-        <MusicCard />
+        <div className='grid grid-cols-1 justify-between gap-x-4 gap-y-10 md:grid-cols-2 lg:grid-cols-4'>
+          {new Array(10).fill(0).map((item, index) => {
+            return <MusicCard key={index} />;
+          })}
+        </div>
       </div>
     </main>
   );
--- src/components/MusicCard.tsx
+++ src/components/MusicCard.tsx
@@ -1,11 +1,11 @@
 'use client';
 import Image from 'next/image';
 import {FaHeart} from 'react-icons/fa6';
-import {BsTranslate} from 'react-icons/bs';
+import {MdOutlineUpdate} from 'react-icons/md';
 
 export default function MusicCard() {
   return (
-    <div className='mt-20 w-[350px] overflow-hidden rounded-lg bg-white '>
+    <div className='mt-20 w-[350px] cursor-pointer overflow-hidden rounded-lg bg-white'>
       <Image
         priority={true}
         src='/default_card.png'
@@ -13,24 +13,34 @@ export default function MusicCard() {
         width={350}
         height={230}
       />
-      <div>
+      <div className='h-[150px] p-5'>
         {/* 카드 윗 줄 */}
-        <div>
-          <div>ko</div>
-          <div>jo</div>
-          <p>가수가 들어갑니다</p>
+        <div className='flex items-center'>
+          <p className='font-medium'>스폰지송</p>
+          <div className='ml-auto flex gap-2'>
+            <div className='inline-block w-11 rounded-3xl bg-[#9ED3FF] py-[0.1rem] text-center text-sm text-white'>
+              KO
+            </div>
+            <div className='inline-block w-11 rounded-3xl bg-[#FF9D9D] py-[0.1rem] text-center text-sm text-white'>
+              JP
+            </div>
+          </div>
         </div>
         {/* 카드 제목 */}
-        <h1>제목이 들어갑니다.</h1>
+        <h1 className='mt-2 text-2xl font-medium'>월요일 좋아</h1>
       </div>
       {/* 작성자 좋아요 */}
-      <div>
-        <BsTranslate />
-        <div>작성자</div>
-      </div>
-      <div>
-        <FaHeart />
-        <div>32</div>
+      <div className='flex border-t px-5 py-3'>
+        <div className='flex items-center gap-1'>
+          <div>
+            <MdOutlineUpdate className='text-lg' />
+          </div>
+          <div className='text-sm'>2024.03.01</div>
+        </div>
+        <div className='ml-auto flex items-center gap-1 text-sm'>
+          <FaHeart className='text-sm' />
+          <div>32</div>
+        </div>
       </div>
     </div>
   );
