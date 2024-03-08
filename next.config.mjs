0712fa25bc76ade099556a5dff36a3957459edab
saemii-24 <hims2419@gmail.com>
2024-03-08 23:25:38 +0900

    Feat: use Query id별 데이터 요청

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_HOST_NAME,
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
