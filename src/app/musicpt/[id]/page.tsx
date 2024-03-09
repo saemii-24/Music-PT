c3b09760818bedd22103f3dc960f104441786d72
saemii-24 <hims2419@gmail.com>
2024-03-10 04:26:45 +0900

    Fix: api call 수정

--- src/app/musicpt/[id]/page.tsx
+++ src/app/musicpt/[id]/page.tsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 
 import Image from 'next/image';
 import Link from 'next/link';
+import React, {useEffect, useState} from 'react';
 
 import {IoPlayCircleOutline} from 'react-icons/io5';
 
@@ -14,41 +15,57 @@ interface ParamsProps {
 }
 
 export default function MusicPt({params}: ParamsProps) {
-  // const id = params.id;
+  const id = params.id;
 
-  // //tanstack query사용
-  // const getMusicData = async () => {
-  //   const {data} = await axios(`/api/music?id=${id}`);
-  //   return data;
-  // };
+  //tanstack query사용
+  const getMusicData = async () => {
+    const {data} = await axios(`/api/music/${id}`);
+    return data;
+  };
 
-  // const {status, data, error} = useQuery({
-  //   queryKey: ['music-pt', id],
-  //   queryFn: getMusicData,
-  // });
+  const {
+    status,
+    data: music,
+    error,
+  } = useQuery({
+    queryKey: ['music-pt', id],
+    queryFn: getMusicData,
+  });
 
-  // let music = data?.posts[0];
+  // // let musicData = data;
+  // const [musicData, setMusicData] = useState<Partial<SupabaseType>>({});
 
-  // console.log();
+  // useEffect(() => {
+  //   setMusicData(music?.post);
+  //   console.log(musicData);
+  // }, [music]);
 
-  // if (status === 'pending') {
-  //   return <span>Loading...</span>;
-  // }
+  if (status === 'pending') {
+    return <span>Loading...</span>;
+  }
 
-  // if (status === 'error') {
-  //   return <span>Error: {error.message}</span>;
-  // }
+  if (status === 'error') {
+    return <span>Error: {error.message}</span>;
+  }
 
   return (
     <div className='flex-1'>
-      <main className='my-20'>
+      {/* <main className='my-20'>
         <section>
           <div className='container flex items-center gap-20 border-b pb-20'>
             <div className='size-[20vw] overflow-hidden rounded-3xl'>
               <Image
                 priority={true}
-                src={'/default_card.png'}
-                alt={'음악'}
+                src={
+                  musicData.kothumbnail
+                    ? musicData.kothumbnail
+                    : '/default_card.png'
+                }
+                alt={
+                  musicData.kotitle
+                    ? musicData.kotitle + '이미지'
+                    : '음악 썸네일'
+                }
                 width={0}
                 height={0}
                 sizes='100vw'
@@ -66,31 +83,47 @@ export default function MusicPt({params}: ParamsProps) {
               <div className='mt-6 flex w-full flex-col gap-1'>
                 <div className='flex'>
                   <p className='w-[100px]'>가수</p>
-                  <p className='w-auto'>전영호</p>
+                  <p className='w-auto'>{musicData.kosinger}</p>
                 </div>
                 <div className='flex'>
                   <p className='w-[100px]'>앨범</p>
-                  <p className='w-auto'>butter-fly 디지몬</p>
+                  <p className='w-auto'>{musicData.koalbum}</p>
                 </div>
                 <div className='flex'>
                   <p className='w-[100px]'>발매년도</p>
-                  <p className='w-auto'>2022</p>
+                  <p className='w-auto'>{musicData.korelease}</p>
                 </div>
                 <div></div>
               </div>
 
               <div className='mt-6 flex gap-6'>
-                <Link
-                  href={''}
-                  className='flex items-center justify-center gap-1 rounded-full border border-music-orange px-4 py-2 text-music-orange transition hover:bg-music-orange hover:text-white'>
-                  한국어 버전
-                  <IoPlayCircleOutline />
-                </Link>
-                <Link
-                  href={'music.jpyoutube'}
-                  className='flex items-center justify-center gap-1 rounded-full border border-music-orange px-4 py-2 text-music-orange transition hover:bg-music-orange hover:text-white'>
-                  일본어 버전 <IoPlayCircleOutline />
-                </Link>
+                {musicData.koyoutube ? (
+                  <Link
+                    href={musicData.koyoutube}
+                    target='_blank'
+                    className='flex items-center justify-center gap-1 rounded-full border border-music-orange px-4 py-2 text-music-orange transition hover:bg-music-orange hover:text-white'>
+                    한국어 버전
+                    <IoPlayCircleOutline />
+                  </Link>
+                ) : (
+                  <div className='flex items-center justify-center gap-1 rounded-full  bg-gray-100 px-4 py-2 transition'>
+                    한국어 버전
+                    <IoPlayCircleOutline />
+                  </div>
+                )}
+                {musicData.jpyoutube ? (
+                  <Link
+                    href={musicData.jpyoutube}
+                    target='_blank'
+                    className='flex items-center justify-center gap-1 rounded-full border border-music-orange px-4 py-2 text-music-orange transition hover:bg-music-orange hover:text-white'>
+                    일본어 버전 <IoPlayCircleOutline />
+                  </Link>
+                ) : (
+                  <div className='flex items-center justify-center gap-1 rounded-full  bg-gray-100 px-4 py-2 transition'>
+                    일본어 버전
+                    <IoPlayCircleOutline />
+                  </div>
+                )}
               </div>
             </div>
           </div>
@@ -98,19 +131,10 @@ export default function MusicPt({params}: ParamsProps) {
 
         <section className='container mt-[10rem]'>
           <div className='relative '>
-            <p className='mb-1'>업데이트: 2023-03-21</p>
+            <p className='mb-1'>업데이트: {musicData?.date?.slice(0, 10)}</p>
             <h2 className='text-4xl font-extrabold'>한국어 버전 가사</h2>
 
             <div className='mt-10 flex gap-6'>
-              {/* <button className='rounded-full border border-music-blue px-6 py-2 text-music-blue'>
-                한국어 버전
-              </button>
-              <button className='rounded-full border border-music-blue px-6 py-2 text-music-blue'>
-                일본어 버전
-              </button>
-              <button className='rounded-full border border-music-blue px-6 py-2 text-music-blue'>
-                가사 비교하기
-              </button> */}
               <ul className='flex gap-3'>
                 <li>한국어 버전</li>
                 <li>일본어 버전</li>
@@ -122,18 +146,18 @@ export default function MusicPt({params}: ParamsProps) {
           </div>
           <article className='mt-10 rounded-lg bg-[#FBFEFF]'>
             <div className='flex flex-col items-center py-20'>
-              <p className='text-lg'>그래 그리 쉽지는 않겠지</p>
-              <p className='mb-8 text-lg text-music-blue'>
-                そんなに簡単ではないでしょう
-              </p>
-              <p className='text-lg'>그래 그리 쉽지는 않겠지</p>
-              <p className='mb-8 text-lg text-music-blue'>
-                そんなに簡単ではないでしょう
-              </p>
+              {musicData?.kolyrics?.split('\n').map((koline, index) => (
+                <React.Fragment key={index}>
+                  <p className='text-lg'>{koline}</p>
+                  <p className='text-lg'>
+                    {musicData?.jplyrics?.split('\n')[index]}
+                  </p>
+                </React.Fragment>
+              ))}
             </div>
           </article>
         </section>
-      </main>
+      </main> */}
     </div>
   );
 }
