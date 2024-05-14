fc228e60eea9a2009ad72f2339aef113f875f499
saemii-24 <hims2419@gmail.com>
2024-05-14 19:22:38 +0900

    Optimize: 이미지 sizes props 설정

--- next.config.mjs
+++ next.config.mjs
@@ -29,7 +29,7 @@ const nextConfig = {
     // deviceSizes: [640, 768, 1024],
     // imageSizes: [128, 256, 384, 512, 768],
     // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [300, 400, 600],
+    imageSizes: [300, 400, 580, 705],
     deviceSizes: [640, 768, 1024],
     remotePatterns: [
       {
