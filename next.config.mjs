950731d5551ed6193e62abc0afaeeaa1042f876e
saemii-24 <hims2419@gmail.com>
2024-05-15 23:57:10 +0900

    Optimize: musicPt Image sizes 조정

--- next.config.mjs
+++ next.config.mjs
@@ -25,10 +25,6 @@ const nextConfig = {
   },
   images: {
     formats: ['image/avif', 'image/webp'],
-    // imageSizes: [300, 700, 400],
-    // deviceSizes: [640, 768, 1024],
-    // imageSizes: [128, 256, 384, 512, 768],
-    // deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [300, 400, 580, 705],
     deviceSizes: [640, 768, 1024],
     remotePatterns: [
